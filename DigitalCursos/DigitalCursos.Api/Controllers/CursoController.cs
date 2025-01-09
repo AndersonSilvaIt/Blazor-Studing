7d361a10f8ee23de58d152f7277c76dd09f9dadd
AndersonSilvaIt <42393231+AndersonSilvaIt@users.noreply.github.com>
2025-01-09 13:02:13 -0300

    Created Curso Repository

---
+++ DigitalCursos/DigitalCursos.Api/Controllers/CursoController.cs
@@ -0,0 +1,116 @@
+﻿using DigitalCursos.Api.Repositories;
+using DigitalCursos.Models.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DigitalCursos.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CursoController: ControllerBase
+    {
+        private readonly ICursoRepository _cursoRepository;
+
+        public CursoController(ICursoRepository cursoRepository)
+        {
+            _cursoRepository = cursoRepository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GetCursos()
+        {
+            try
+            {
+                var result = await _cursoRepository.GetCursos();
+                return Ok(result);
+
+            } catch(Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                    "Erro ao retornar os dados do banco de dados");
+            }
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<Curso>> GetCurso(int id)
+        {
+            try
+            {
+                var result = await _cursoRepository.GetCurso(id);
+                return Ok(result);
+
+            } catch(Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                    "Erro ao retornar os dados do banco de dados");
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Curso>> GetCurso(Curso curso)
+        {
+            try
+            {
+                if(curso == null)
+                {
+                    return BadRequest();
+                }
+                var createdCurso = await _cursoRepository.AddCurso(curso);
+
+                return CreatedAtAction(nameof(GetCurso), new { id = createdCurso.CursoId }, createdCurso);
+
+            } catch(Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                    "Erro ao retornar os dados do banco de dados");
+            }
+        }
+
+        [HttpPut("{id:int}")]
+        public async Task<ActionResult<Curso>> UpdateCurso(int id, Curso curso)
+        {
+            try
+            {
+                if(id != curso.CursoId)
+                {
+                    return BadRequest($"O curso com id={id} não confere com o curso a ser atualizado");
+                }
+
+                var cursoToUpdate = await _cursoRepository.GetCurso(id);
+
+                if(cursoToUpdate == null)
+                {
+                    return NotFound($"Curso com o id = {id} não encontrado");
+                }
+
+                return await _cursoRepository.UpdateCurso(curso);
+
+            } catch(Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                    "Erro ao retornar os dados do banco de dados");
+            }
+        }
+
+
+        [HttpDelete("{id:int}")]
+        public async Task<ActionResult<Curso>> DleteCurso(int id)
+        {
+            try
+            {
+                var cursoToDelete = await _cursoRepository.GetCurso(id);
+                if(cursoToDelete == null)
+                {
+                    return NotFound($"Curso com o id = {id} não encontrado");
+                }
+
+                return await _cursoRepository.DeleteCurso(id);
+
+            } catch(Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                    "Erro ao retornar os dados do banco de dados");
+            }
+        }
+
+    }
+}
---
+++ DigitalCursos/DigitalCursos.Api/Repositories/CursoRepository.cs
@@ -0,0 +1,71 @@
+﻿using DigitalCursos.Api.Context;
+using DigitalCursos.Models.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DigitalCursos.Api.Repositories
+{
+    public class CursoRepository: ICursoRepository
+    {
+        private readonly AppDbContext _context;
+        public CursoRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Curso> AddCurso(Curso curso)
+        {
+            var result = await _context.Cursos.AddAsync(curso);
+            await _context.SaveChangesAsync();
+            return result.Entity;
+        }
+
+        public async Task<Curso> DeleteCurso(int id)
+        {
+            var curso = await _context.Cursos.FirstOrDefaultAsync(c => c.CursoId == id);
+            if(curso != null)
+            {
+                _context.Cursos.Remove(curso);
+                await _context.SaveChangesAsync();
+                return curso;
+            }
+
+            return null;
+        }
+
+        public async Task<Curso> GetAlunosCurso(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Curso> GetCurso(int id)
+        {
+            var curso = await _context.Cursos.FirstOrDefaultAsync(c => c.CursoId == id);
+            return curso;
+        }
+
+        public async Task<IEnumerable<Curso>> GetCursos()
+        {
+            return await _context.Cursos.AsNoTracking().ToListAsync();
+        }
+
+        public async Task<Curso> UpdateCurso(Curso curso)
+        {
+            /*var result = await _context.Cursos.FirstOrDefaultAsync(c => c.CursoId == curso.CursoId);
+            if(result != null)
+            {
+                result.Nome = curso.Nome;
+                result.Sobrenome = curso.Sobrenome;
+                result.Email = curso.Email;
+                result.Nascimento = curso.Nascimento;
+                result.Foto = curso.Foto;
+                result.Genero = curso.Genero;
+                result.CursoId = curso.CursoId;
+
+                await _context.SaveChangesAsync();
+                return result;
+            }*/
+
+            return null;
+        }
+    }
+}
